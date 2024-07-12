2654e824235080daa38bb65ab361fe0ca5ec8754
Brijesh Patel <brijeshpatel96@gmail.com>
2024-07-12 11:40:45 +0100

    adding remaining files

---
+++ src/firebase.jsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_REACT_APP_MESSAGING_SENDER,
+  appId: process.env.VITE_REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
+export const storage = getStorage();
+export const db = getFirestore()
