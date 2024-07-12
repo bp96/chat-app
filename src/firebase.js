8bdf29b83ef97fa2e3e0b4d3dc8f80f1dc5da3eb
Brijesh Patel <brijeshpatel96@gmail.com>
2024-07-12 12:40:10 +0100

    commenting unused code

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
 export const storage = getStorage();
 export const db = getFirestore()
